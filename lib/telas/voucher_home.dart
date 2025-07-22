19a0b4e01b10062598d1aa7969a186e162e50bd3
MaxTrsD <maxneydomingos@gmail.com>
2025-07-22 16:18:25 -0300

    Outro commit

---
+++ lib/telas/voucher_home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class voucher_home extends StatelessWidget {
+  const voucher_home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        backgroundColor: Colors.red[400],
+        elevation: 10.5,
+        shadowColor: Colors.black,
+      ),
+    );
+  }
+}
---
+++ lib/telas/voucher_tela.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class tela_voucher extends StatelessWidget {
+  const tela_voucher({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
